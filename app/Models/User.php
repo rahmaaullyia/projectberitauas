0dccf1370422326cc0eca18f34626f6af71c48a8
nadia022 <nadiayayaa02@gmail.com>
2026-06-23 16:47:34 +0700

    feat(mhs3): add public function comments()

--- app/Models/User.php
+++ app/Models/User.php
@@ -39,7 +39,10 @@ class User extends Authenticatable
         return $this->hasMany(SavedNews::class);
     }
 
-    
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
 
     // ─── Accessors ────────────────────────────────────────────────────────────
 
