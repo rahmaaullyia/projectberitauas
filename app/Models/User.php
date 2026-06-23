bbded8b19667e075aa8a34480ebd9bfe2505d93a
rahmaaullyia <rahmaaullyia@gmail.com>
2026-06-23 15:01:30 +0700

    update casts

--- app/Models/User.php
+++ app/Models/User.php
@@ -25,6 +25,12 @@ class User extends Authenticatable
         'remember_token',
     ];
 
+    protected $casts = [
+        'email_verified_at'   => 'datetime',
+        'password'            => 'hashed',
+        'is_active'           => 'boolean',
+        'is_admin'            => 'boolean',
+    ];
 
     /**
      * Get the attributes that should be cast.
