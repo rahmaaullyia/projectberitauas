af0fb6764808c82031d8265e1e6b6cd5778d624b
rahmaaullyia <rahmaaullyia@gmail.com>
2026-06-23 14:44:44 +0700

    hapus usser di saved news

--- app/Models/SavedNews.php
+++ app/Models/SavedNews.php
@@ -15,11 +15,5 @@ class SavedNews extends Model
         'user_id',
         'news_id',
     ];
-
-    // ─── Relationships ────────────────────────────────────────────────────────
-
-    public function user()
-    {
-        return $this->belongsTo(User::class);
-    }
+   
 }
